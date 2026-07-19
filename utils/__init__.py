9c3ca158078a0d06fafb898b3544d7ba9e1c2d42
manav-2812 <manavraj854@gmail.com>
2026-07-19 18:55:22 +0530

    refactor(utils): update package-level utilities init imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,8 @@
 # utils package
-from utils.decorators import admin_required, role_required, owner_required
+from utils.decorators import admin_required, owner_required
 from utils.helpers import food_image_url, format_currency, register_template_globals
 
 __all__ = [
-    'admin_required', 'role_required', 'owner_required',
+    'admin_required', 'owner_required',
     'food_image_url', 'format_currency', 'register_template_globals',
 ]
